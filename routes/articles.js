79a700f36a338f60bb35c80836d59eee00c2df69
DanieljM95 <101828972+DanieljM95@users.noreply.github.com>
2022-05-26 15:35:38 -0400

    Update articles.js
    update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -33,20 +33,20 @@ router.delete('/:id', async (req, res) => {
     res.redirect('/')
 })
 
-function saveArticleAndRedirect(path) {
-    return async (req, res) => {
-        let article = req.article
-        article.title = req.body.title,
-            article.description = req.body.description,
-            article.markdown = req.body.markdown
-        try {
-            article = await article.save()
-            res.redirect(`/articles/${article.slug}`)
-        } catch (e) {
-            console.log(e)
-            res.render(`articles/${path}`, { article: article })
-        }
+
+return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title,
+        article.description = req.body.description,
+        article.markdown = req.body.markdown
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+        console.log(e)
+        res.render(`articles/${path}`, { article: article })
     }
+}
 }
 
 module.exports = router
